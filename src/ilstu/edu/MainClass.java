192193c27c425f58ae84aa1e319f3d05e3c79a58
aslerch <88261471+aslerch@users.noreply.github.com>
2021-08-27 14:39:45 -0500

    Completed Project

--- src/ilstu/edu/MainClass.java
+++ src/ilstu/edu/MainClass.java
@@ -1,13 +1,125 @@
 package ilstu.edu;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MainClass {
+
+    private static Scanner keyboard = new Scanner(System.in);
+    private static final String WELCOME_BANNER = """
+            *****************************************
+            Welcome to the School Statistics Database
+            *****************************************""";
+    private static final int PROCESS_FILE = 1, PRINT_STUDENTS = 2, GENERATE_REPORT_CARD = 3, DISPLAY_STATISTICS = 4, EXIT = 5;
+    private static StudentReport studentReport = new StudentReport();
 
     public static void main(String [] args) {
 
-        StudentReport stuRep = new StudentReport("Grades.csv");
-        stuRep.readFile();
-        stuRep.writeFile("Hannah Abbott");
+        System.out.println(WELCOME_BANNER);
+        int menuSelection = -1; //arbitrary initial value for menuSelection
+        while ( menuSelection != EXIT) {
+            displayMenu();
+            try {
+                menuSelection = askForIntBetween(1, 5); //menu selections have the bounds of 1 and 5
+                if (menuSelection == PROCESS_FILE) {
+                    studentReport = new StudentReport(askForStringWithinLength("Please enter the file name: ", 100));
+                    studentReport.readFile();
+                    System.out.println("*****************************************");
+                }
+                if (menuSelection == PRINT_STUDENTS) {
+                    if (studentReport.isProcessed) {
+                        studentReport.printStudents();
+                    }
+                    if (studentReport.isProcessed == false)
+                        System.out.println("ERROR: file has not yet been processed");
+                    System.out.println("*****************************************");
+                }
+                if (menuSelection == GENERATE_REPORT_CARD) {
+                    if (studentReport.isProcessed) {
+                        String studentName = askForStringWithinLength("Please enter the name of a student: ", 100);
+                        studentReport.writeFile(studentName);
+                    }
+                    if (studentReport.isProcessed == false)
+                        System.out.println("ERROR: file has not yet been processed");
+                    System.out.println("*****************************************");
 
+                }
+                if (menuSelection == DISPLAY_STATISTICS) {
+                    if (studentReport.isProcessed)
+                        studentReport.displayStatistics();
+                    if (studentReport.isProcessed == false)
+                        System.out.println("ERROR: file has not yet been processed");
+                    System.out.println("*****************************************");
+                }
+                if (menuSelection == EXIT)
+                    System.out.println("Thank you for using the school statistics database");
+            } catch (InputMismatchException e) {
+                keyboard.nextLine();
+                System.out.println("ERROR: Input not an integer");
+            }
+        }
+    }
+
+    /**
+     * Displays the menu
+     */
+    private static void displayMenu() {
+        System.out.println("""
+                1. Process a file
+                2. Print a list of all students
+                3. Generate a report card
+                4. Display statistics
+                5. Exit""");
+    }
+
+    /**
+     * Asks for an int between or equal to lower and upper bound values
+     * @param num1 The lower bound
+     * @param num2 The upper bound
+     * @return A number between or equal to the upper bound and lower bound
+     */
+    private static int askForIntBetween(int num1, int num2) {
+        int selection = -9999;
+        boolean isInt = false;
+        boolean isBetweenNum1AndNum2 = false;
+        while (isInt == false || isBetweenNum1AndNum2 == false) {
+            isInt = false;
+            isBetweenNum1AndNum2 = false;
+            try {
+                System.out.print("Your selection (" + num1 + " - " + num2 + "): ");
+                selection = keyboard.nextInt();
+                if (selection >= num1 & selection <= num2)
+                    isBetweenNum1AndNum2 = true;
+                if (selection < num1 || selection > num2)
+                    System.out.println("selection outside of acceptable range");
+                isInt = true;
+            } catch (InputMismatchException e) {
+                keyboard.nextLine();
+                System.out.println("Incorrect input: not an integer");
+            }
+        }
+        return selection;
+    }
+
+    /**
+     * Asks for a String that is within a certain length
+     * @param informationRequestMessage The request message
+     * @param lengthUpperBound The upper bound for length
+     * @return A String within a length determined by the upper bound
+     */
+    private static String askForStringWithinLength(String informationRequestMessage, int lengthUpperBound) {
+        keyboard.nextLine();
+        boolean isWithinCorrectLength = false;
+        String output = "";
+        while (isWithinCorrectLength == false) {
+            System.out.print(informationRequestMessage);
+            output = keyboard.nextLine();
+            if (output.length() <= lengthUpperBound)
+                isWithinCorrectLength = true;
+            if (output.length() > lengthUpperBound)
+                System.out.println("input not of valid length " + "(" + lengthUpperBound + ")");
+        }
+        return output;
     }
 
 }
--- src/ilstu/edu/StudentReport.java
+++ src/ilstu/edu/StudentReport.java
@@ -3,12 +3,15 @@ package ilstu.edu;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /** A class that represents a student report */
 public class StudentReport {
 
     private final int INVALID = -1;
+    private final int NUM_OF_ASSIGNMENTS = 8;
 
     /** Name of the file that we want to read */
     private String fileName;
@@ -22,6 +25,12 @@ public class StudentReport {
     /** Names of all the graded items in the file(fileName) */
     private String [] gradedItems;
 
+    /** true if field fileName has a file associated with it, false if field fileName is null */
+    public boolean isProcessed = false;
+
+    /** Default constructor */
+    public StudentReport() {};
+
     /**
      * Instantiates a StudentReport object and also instantiates the fields of grades, students, and gradedItems
      * @param fileName The name of the file that contains the data to be used
@@ -33,88 +42,52 @@ public class StudentReport {
         this.gradedItems = new String [8];
     }
 
+    /** Methods */
     /**
-     * Reads fileName and fills the grades, students, and gradedItems arrays
+     * Calculates the average score for class
+     * @return The average score for the class
      */
-    public void readFile() {
-        File gradesFile = new File(this.fileName);
-        fillGradesArray(gradesFile);
-        fillStudentsArray(gradesFile);
-        fillGradedItemsArray(gradesFile);
-    }
-
-    /**
-     * fills the gradedItems array
-     * @param gradesFile The name of the File object that references fileName
-     */
-    private void fillGradedItemsArray(File gradesFile) {
-        try {
-            Scanner fileReader = new Scanner(gradesFile);
-            String currentLine = fileReader.nextLine();
-            String [] gradesFileHeaders = currentLine.split(",");
-            for (int i = 1; i < gradesFileHeaders.length; i++) //skips the "name" header
-                this.gradedItems[i - 1] = gradesFileHeaders[i];
-        } catch (FileNotFoundException e) {
-            System.out.println("file cannot be found");
-        }
-    }
-
-    /**
-     * Fills the grades array
-     * @param gradesFile The name of the File object that references fileName
-     */
-    private void fillGradesArray(File gradesFile) {
-        try {
-            Scanner fileReader = new Scanner(gradesFile);
-            fileReader.nextLine(); //skips the header
-            int rowIndex = 0;
-            while (fileReader.hasNextLine()) {
-                String currentLine = fileReader.nextLine();
-                String [] grades = currentLine.split(",");
-                for (int i = 1; i < grades.length; i++) {
-                    this.grades[rowIndex][i - 1] = Double.parseDouble(grades[i]);
-                }
-                rowIndex++;
+    private double calculateAverageForClass() {
+        int numOfStudents = findNumOfStudents();
+        double average;
+        double totalScore = 0;
+        int numOfScores = 0;
+        for (int i = 0; i < numOfStudents; i++) {
+            for (int j = 0; j < grades[i].length; j++) {
+                totalScore += grades[i][j];
+                numOfScores++;
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("file cannot be found");
         }
+        average = totalScore/(double)(numOfStudents);
+        return average;
     }
 
     /**
-     * Fills the students array
-     * @param gradesFile The name of the File object that references fileName
+     * Calculates the total score for all grades of a student
+     * @param name The name of the student
+     * @return The total score
      */
-    private void fillStudentsArray(File gradesFile) {
-        try {
-            Scanner fileReader = new Scanner(gradesFile);
-            fileReader.nextLine(); //skips the header
-            int studentsIndex = 0;
-            while (fileReader.hasNextLine()) {
-                String currentLine = fileReader.nextLine();
-                String [] info = currentLine.split(",");
-                students[studentsIndex] = info[0];
-                studentsIndex++;
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("file cannot be found");
+    private double calculateTotalForGradesOfStudent(String name) {
+        double total = 0;
+        int indexOfStudent = findIndexOfStudent(name);
+        for (int i = 0; i < grades[indexOfStudent].length; i++) {
+            total += (int)grades[indexOfStudent][i];
         }
+        return total;
     }
 
     /**
-     * Generates a report card for a specific student and saves it as "(student's name).txt"
+     * Collects the grades and assignment names for a student
+     * @param name The name of the student
+     * @return A String in the format "(name of assignment): (grade)" for all assignments
      */
-    public void writeFile(String name) {
-        String reportCardInfo = createReportCardInfo(name);
-        String outputFileName = name.replace(" ", "") + ".txt";
-        File reportCard = new File(outputFileName);
-        try {
-            PrintWriter fileWriter = new PrintWriter(reportCard);
-            fileWriter.write(reportCardInfo);
-            fileWriter.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("cannot write to the file");
+    private String collectGradesWithAssignmentNameOfStudent(String name) {
+        String gradesOfStudent = "";
+        int indexOfStudent = findIndexOfStudent(name);
+        for (int i = 0; i < grades[indexOfStudent].length; i++) {
+            gradesOfStudent += gradedItems[i] + ": " + (int)grades[indexOfStudent][i] + "\n";
         }
+        return gradesOfStudent;
     }
 
     /**
@@ -137,50 +110,6 @@ public class StudentReport {
     }
 
     /**
-     * Finds the index of a student in the students array
-     * @param name The name of the student
-     * @return The index of the student if the student is in the students array and -1 otherwise
-     */
-    private int findIndexOfStudent(String name) {
-        int index = -1;
-        for (int i = 0; i < students.length; i++) {
-            if (students[i] != null) {
-                if (students[i].equalsIgnoreCase(name))
-                    index = i;
-            }
-        }
-        return index;
-    }
-
-    /**
-     * Collects the grades and assignment names for a student
-     * @param name The name of the student
-     * @return A String in the format "(name of assignment): (grade)" for all assignments
-     */
-    private String collectGradesWithAssignmentNameOfStudent(String name) {
-        String gradesOfStudent = "";
-        int indexOfStudent = findIndexOfStudent(name);
-        for (int i = 0; i < grades[indexOfStudent].length; i++) {
-            gradesOfStudent += gradedItems[i] + ": " + (int)grades[indexOfStudent][i] + "\n";
-        }
-        return gradesOfStudent;
-    }
-
-    /**
-     * Calculates the total score for all grades of a student
-     * @param name The name of the student
-     * @return The total score
-     */
-    private double calculateTotalForGradesOfStudent(String name) {
-        double total = 0;
-        int indexOfStudent = findIndexOfStudent(name);
-        for (int i = 0; i < grades[indexOfStudent].length; i++) {
-            total += (int)grades[indexOfStudent][i];
-        }
-        return total;
-    }
-
-    /**
      * Determines the letter grade that a student should earn based on their total score
      * @param name The name of the student
      * @return The student's letter grade
@@ -199,6 +128,181 @@ public class StudentReport {
         if (total < 69)
             grade = 'F';
         return grade;
+    }
+
+    /**
+     * Displays information relating to the class average, highest total score, and lowest total score.
+     */
+    public void displayStatistics() {
+        DecimalFormat decimalFormat = new DecimalFormat("##.##");
+        String studentWithHighestTotalScore = findStudentWithHighestTotalScore();
+        String studentWithLowestTotalScore = findStudentWithLowestTotalScore();
+        String output = "Average Score of the Class: " + decimalFormat.format(calculateAverageForClass()) + "%" +
+                "\nStudent with the highest total score: " + studentWithHighestTotalScore + "(" + (int)calculateTotalForGradesOfStudent(studentWithHighestTotalScore) + ")" +
+                "\nStudent with the lowest total score: " + studentWithLowestTotalScore + "(" + (int)calculateTotalForGradesOfStudent(studentWithLowestTotalScore) + ")";
+        System.out.println(output);
+    }
+
+    /**
+     * fills the gradedItems array
+     * @param gradesFile The name of the File object that references fileName
+     */
+    private void fillGradedItemsArray(File gradesFile) {
+        try {
+            Scanner fileReader = new Scanner(gradesFile);
+            String currentLine = fileReader.nextLine();
+            String [] gradesFileHeaders = currentLine.split(",");
+            for (int i = 1; i < gradesFileHeaders.length; i++) //skips the "name" header
+                this.gradedItems[i - 1] = gradesFileHeaders[i];
+            System.out.println("File successfully read");
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found");
+        }
+    }
+
+    /**
+     * Fills the grades array
+     * @param gradesFile The name of the File object that references fileName
+     */
+    private void fillGradesArray(File gradesFile) {
+        try {
+            Scanner fileReader = new Scanner(gradesFile);
+            fileReader.nextLine(); //skips the header
+            int rowIndex = 0;
+            while (fileReader.hasNextLine()) {
+                String currentLine = fileReader.nextLine();
+                String [] grades = currentLine.split(",");
+                for (int i = 1; i < grades.length; i++) {
+                    this.grades[rowIndex][i - 1] = Double.parseDouble(grades[i]);
+                }
+                rowIndex++;
+                isProcessed = true;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("");
+        }
+    }
+
+    /**
+     * Fills the students array
+     * @param gradesFile The name of the File object that references fileName
+     */
+    private void fillStudentsArray(File gradesFile) {
+        try {
+            Scanner fileReader = new Scanner(gradesFile);
+            fileReader.nextLine(); //skips the header
+            int studentsIndex = 0;
+            while (fileReader.hasNextLine()) {
+                String currentLine = fileReader.nextLine();
+                String [] info = currentLine.split(",");
+                students[studentsIndex] = info[0];
+                studentsIndex++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("");
+        }
+    }
+
+    /**
+     * Finds the student with the highest total score
+     * @return The student's name
+     */
+    private String findStudentWithHighestTotalScore() {
+        int highestTotal = (int)calculateTotalForGradesOfStudent(students[0]);
+        String name = students[0];
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] != null) {
+                if ( (int)calculateTotalForGradesOfStudent(students[i]) > highestTotal) {
+                    highestTotal = (int)calculateTotalForGradesOfStudent(students[i]);
+                    name = students[i];
+                }
+            }
+        }
+        return name;
+    }
+
+    /**
+     * Finds the student with the lowest total score
+     * @return The student's name
+     */
+    private String findStudentWithLowestTotalScore() {
+        int lowestTotal = (int)calculateTotalForGradesOfStudent(students[0]);
+        String name = students[0];
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] != null) {
+                if ( (int)calculateTotalForGradesOfStudent(students[i]) < lowestTotal) {
+                    lowestTotal = (int)calculateTotalForGradesOfStudent(students[i]);
+                    name = students[i];
+                }
+            }
+        }
+        return name;
+    }
+
+    /**
+     * Finds the index of a student in the students array
+     * @param name The name of the student
+     * @return The index of the student if the student is in the students array and -1 otherwise
+     */
+    private int findIndexOfStudent(String name) {
+        int index = -1;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] != null) {
+                if (students[i].equalsIgnoreCase(name))
+                    index = i;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Finds the number of students
+     * @return The number of students
+     */
+    private int findNumOfStudents() {
+        int numOfStudents = 0;
+        for (String student : students) {
+            if (student != null)
+                numOfStudents++;
+        }
+        return numOfStudents;
+    }
+
+    /**
+     * Prints a list of the students in the students array
+     */
+    public void printStudents() {
+        System.out.println("Students: ");
+        for (String name : students) {
+            if (name != null)
+                System.out.println(name);
+        }
+    }
+
+    /**
+     * Reads fileName and fills the grades, students, and gradedItems arrays
+     */
+    public void readFile() {
+        File gradesFile = new File(this.fileName);
+        fillGradesArray(gradesFile);
+        fillStudentsArray(gradesFile);
+        fillGradedItemsArray(gradesFile);
+    }
+
+    /**
+     * Generates a report card for a specific student and saves it as "(student's name).txt"
+     */
+    public void writeFile(String name) {
+        String reportCardInfo = createReportCardInfo(name);
+        String outputFileName = name.replace(" ", "") + ".txt";
+        File reportCard = new File(outputFileName);
+        try {
+            PrintWriter fileWriter = new PrintWriter(reportCard);
+            fileWriter.write(reportCardInfo);
+            fileWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("cannot write to the file");
+        }
     }
 
 }
